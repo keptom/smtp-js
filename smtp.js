f0aacb4faadb03fb2b2d9674eddef0bee1238137
killnwak <keptom135@gmail.com>
2021-04-02 14:21:39 -0400

    kii

--- smtp.js
+++ smtp.js
@@ -1,12 +1,24 @@
+$(document).ready(function () {
+    $("form").submit(function (event) {
+      var formData = {
+        name: $("#name").val(),
+        email: $("#email").val(),
+        superheroAlias: $("#superheroAlias").val(),
+      };
+  
+      $.ajax({
+        type: "POST",
+        url: "process.php",
+        data: formData,
+        dataType: "json",
+        encode: true,
+      }).done(function (data) {
+        console.log(data);
+      });
+  
+      event.preventDefault();
+    });
+  });
 
-dsdsdd
-cscsdf
-dd
-d
-d
+  
 
-d
-
-d
-d
-fffffffff
