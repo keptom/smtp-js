bd2559d33926c46d709132221c287e0f6c643d1a
killnwak <keptom135@gmail.com>
2021-04-03 04:32:47 -0400

    now

--- smtp.js
+++ smtp.js
@@ -1,16 +1,12 @@
-$("#my_form").submit(function(event){
-  event.preventDefault(); //prevent default action 
-  var post_url = ""; //get form action url
-  var request_method = $(this).attr("post"); //get form GET/POST method
-var form_data = new FormData(this); //Creates new FormData object
-  $.ajax({
-      url : post_url,
-      type: request_method,
-      data : form_data,
-  contentType: false,
-  cache: false,
-  processData:false
-  }).done(function(response){ //
-      $("#server-results").html(response);
+function send(subject,sendername,msg) {
+  $.post("demo_test_post.asp",
+  {
+    sbj: subject,
+    sender: sendername,
+    message: msg
+  },
+  function(data, status){
+    alert("Data: " + data + "\nStatus: " + status);
   });
-});
+
+}
