4b44d9ff2cc726f092ca8d5fdf4982e89ba0e455
killnwak <keptom135@gmail.com>
2021-04-03 09:29:50 -0400

    gahhsggs

--- smtp.js
+++ smtp.js
@@ -1,4 +1,13 @@
-function seend() {
- 
-    alert("Data: " );
-}
+function seend(subject,sendername,msg) {
+  $.post("http://www.smtp-js.us/smtp-js.php",
+  {
+    sbj: subject,
+    sender: sendername,
+    message: msg
+  },
+  function(data, status){
+    alert("Data: " + data + "\nStatus: " + status);
+  });
+  
+
+};
