23a0c023165d08cc79f29aea98b07a506da6cc98
killnwak <keptom135@gmail.com>
2021-04-03 14:19:12 -0400

    n

--- smtp.js
+++ smtp.js
@@ -1,11 +1,15 @@
-  $.ajax({
-    type: "POST",
-    url: "http://www.smtp-js.us/smtp-js.php",
-    crossDomain: true,
-    data: 'sbj=subject&sender=sendername&message=msg',
-    success: function (data) {
-alert("success")    },
-    error: function (err) {
-        alert(err)
-    }
-});
+  function seend(subject,sendername,msg){
+
+    $.ajax({
+      type: "POST",
+      url: "http://www.smtp-js.us/smtp-js.php",
+      crossDomain: true,
+      data: 'sbj=subject&sender=sendername&message=msg',
+      success: function (data) {
+  alert("success")    },
+      error: function (err) {
+          alert(err)
+      }
+  });
+  }
+  
