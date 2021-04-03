6d7d8afc9625d46bfe40e03695dbfe6cee448dd5
killnwak <keptom135@gmail.com>
2021-04-03 08:27:08 -0400

    jei

--- smtp.js
+++ smtp.js
@@ -1,12 +1,16 @@
 function send(subject,sendername,msg) {
-  $.post("http://www.smtp-js.us/smtp-js.php",
-  {
-    sbj: subject,
-    sender: sendername,
-    message: msg
-  },
-  function(data, status){
-    alert("Data: " + data + "\nStatus: " + status);
-  });
+   var post_url = "http://www.smtp-js.us/smtp-js.php"; //get form action url
+    var request_method = "POST"; //get form GET/POST method
+	var form_data = new FormData(this); //Creates new FormData object
+    $.ajax({
+        url : post_url,
+        type: request_method,
+        data : form_data,
+		contentType: false,
+		cache: false,
+		processData:false
+    }).done(function(response){ //
+        $("#server-results").html(response);
+    });
 
 }
