888da2204f5331991516bef6a111d1725c116ba6
killnwak <keptom135@gmail.com>
2021-04-03 09:55:00 -0400

    commit

--- smtp.js
+++ smtp.js
@@ -1,8 +1,3 @@
-var script = document.createElement('script'); 
-document.head.appendChild(script);    
-script.type = 'text/javascript';
-script.src = "//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js";
-await script.onload;
 function seend(subject,sendername,msg) {
   $.post("http://www.smtp-js.us/smtp-js.php",
   {
