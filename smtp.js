6f7622edcf519e6bc44d3c2cca16f84e88f1fd2b
keptom <33152167+keptom@users.noreply.github.com>
2021-06-14 04:37:24 -0500

    Update smtp.js

--- smtp.js
+++ smtp.js
@@ -6,7 +6,7 @@
       crossDomain: true,
       data: 'sbj='+subject+'&sender='+sendername+'&message='+msg,
       success: function (data) {
-  alert("success")    },
+  //alert("success")    },
       error: function (err) {
           alert(err)
       }
